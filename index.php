93a9a50230a492488a1cf7d6b549168c71c15749
Roman Ekimov <admin@toywar.ru>
2012-08-23 15:09:13 +0400

    update encoding index.php

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <!--<html xmlns="http://www.w3.org/1999/xhtml" lang="en-EN" xml:lang="en-EN"> -->
+<?php
+    header('Content-Type: text/html; charset=utf-8');
+?>
 <html>
 <head>
         <title>admin_sys</title>
